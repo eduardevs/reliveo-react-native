9ba20373fa837009bb882ea39e39552bb0a0db1a
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-05-17 16:34:31 +0200

    profile picture

---
+++ src/screens/profile/index.js
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import Profilepicture from '../../components/profilepicture'
+
+
+export default function ProfileScreen() {
+  return (
+    <View>
+      <Profilepicture></Profilepicture>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
---
+++ src/screens/profile/styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native'
+import React from 'react'
+
+const styles = StyleSheet.create({
+	
+});
+
+export default styles
