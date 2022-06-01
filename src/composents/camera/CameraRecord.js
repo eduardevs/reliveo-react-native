76af0319efac008a45f1017b21ff29b02dcb96c6
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2022-06-01 20:50:42 +0200

    Patch caméra

--- src/composents/camera/CameraRecord.js
+++ src/composents/camera/CameraRecord.js
@@ -1,8 +1,7 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image, Button } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { Camera } from 'expo-camera'
-import { Feather } from '@expo/vector-icons'
-
+import {StyleSheet, Text, View, TouchableOpacity, Image, Button} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import {Camera} from 'expo-camera'
+import {Feather} from '@expo/vector-icons'
 
 
 import styles from './styles'
@@ -13,71 +12,74 @@ export default function CameraRecord({setRecord, setRecordFinish, record}) {
 
     const [cameraType, setCameraType] = useState(Camera.Constants.Type.back)
     const [cameraFlash, setCameraFlash] = useState(Camera.Constants.FlashMode.off)
-  
+
     const [isCameraRedy, setIsCameraRedy] = useState(false)
 
 
-
     const recordVideo = async () => {
-      if(cameraRef) {
-        try {
-          const options = {maxDuration: 10, quality: Camera.Constants.VideoQuality['480']}
-          const videoRecordPromise = cameraRef.recordAsync(options)
-          setRecordFinish(false)
-          if(videoRecordPromise) {
-            const data = await videoRecordPromise;
-            setRecord(data.uri)
-          }
-        } catch (error) {
-          console.warn(error)
+        if (cameraRef) {
+            try {
+                const options = {maxDuration: 10, quality: Camera.Constants.VideoQuality['480']}
+                const videoRecordPromise = cameraRef.recordAsync(options)
+                setRecordFinish(false)
+                if (videoRecordPromise) {
+                    const data = await videoRecordPromise;
+                    setRecord(data.uri)
+                }
+            } catch (error) {
+                console.warn(error)
+            }
         }
-      }
     }
 
 
     const stopVideo = async () => {
-      if(cameraRef) {
-        cameraRef.stopRecording()
-        if(record) {
-          setRecordFinish(true)
+        if (cameraRef) {
+            cameraRef.stopRecording()
+            if (record) {
+                setRecordFinish(true)
+            }
         }
-      }
     }
     return (
         <>
-        {/* Section pour enregitrer une vidéo */}
-          <Camera 
-            ref={ref => setCameraRef(ref)}
-            style={styles.camera}
-            ratio={'16:9'}
-            type={cameraType}
-            flashMode={cameraFlash}
-            onCameraReady={() => setIsCameraRedy(true)}
-          />
-          <View style={styles.sideBarContainer}>
-            <TouchableOpacity 
-              style={styles.sideBarButton}
-              onPress={() => setCameraType(cameraType === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back)}
-              >
-              <Feather name="refresh-ccw" size={24} color={'#A65AFF'}/>
-            </TouchableOpacity>
-            <TouchableOpacity 
-              style={styles.sideBarButton}
-              onPress={() => setCameraFlash(cameraFlash === Camera.Constants.FlashMode.off ? Camera.Constants.FlashMode.torch : Camera.Constants.FlashMode.off)}
-              >
-              <Feather name="zap" size={24} color={'#A65AFF'}/>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.bottomBarContainer}>
-              <View style={styles.recordButtonContainer}>
-                <TouchableOpacity 
-                  disabled={!isCameraRedy}
-                  onLongPress={() => recordVideo()}
-                  onPressOut={() => stopVideo()}
-                  style={styles.recordButton}
+            {/* Section pour enregitrer une vidéo */}
+            <View style={styles.cameraContainer}>
+                <Camera
+                    ref={ref => setCameraRef(ref)}
+                    style={styles.camera}
+                    ratio={'16:9'}
+                    type={cameraType}
+                    flashMode={cameraFlash}
+                    onCameraReady={() => setIsCameraRedy(true)}
                 />
+                <View style={styles.buttonCameraContainer}>
+                    <View style={styles.sideBarContainer}>
+                        <TouchableOpacity
+                            style={styles.sideBarButton}
+                            onPress={() => setCameraType(cameraType === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back)}
+                        >
+                            <Feather name="refresh-ccw" size={24} color={'#A65AFF'}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.sideBarButton}
+                            onPress={() => setCameraFlash(cameraFlash === Camera.Constants.FlashMode.off ? Camera.Constants.FlashMode.torch : Camera.Constants.FlashMode.off)}
+                        >
+                            <Feather name="zap" size={24} color={'#A65AFF'}/>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.bottomBarContainer}>
+                        <View style={styles.recordButtonContainer}>
+                            <TouchableOpacity
+                                disabled={!isCameraRedy}
+                                onLongPress={() => recordVideo()}
+                                onPressOut={() => stopVideo()}
+                                style={styles.recordButton}
+                            />
+                        </View>
+                    </View>
+                </View>
             </View>
-          </View>
         </>
     )
 }
--- src/composents/camera/styles.js
+++ src/composents/camera/styles.js
@@ -1,57 +1,60 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from "react-native";
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'black',
+        width:"100%",
+        height:"100%",
     },
     containerNo: {
         display: "none",
     },
+    cameraContainer: {
+        flex: 1,
+        width: "100%",
+        height:"100%",
+        position:"relative",
+    },
+
     camera: {
         flex: 1,
-        aspectRatio: 9/16,
+        aspectRatio: 9 / 16,
+
+    },
+    buttonCameraContainer: {
+        flex:1,
+        position:"absolute",
+        width:"100%",
+        height:"100%",
     },
     bottomBarContainer: {
         alignItems: "center",
         position: 'absolute',
-        bottom:0,
-        flexDirection:'row',
-        marginBottom:30,
+        bottom: 0,
+        flexDirection: 'row',
+        marginBottom: 30,
     },
     recordButtonContainer: {
-        flex:1,
-        marginHorizontal: 30,
+        width:"100%",
+        flex: 1,
+        alignItems:"center",
+        justifyContent:"center",
+        bottom:0,
     },
     recordButton: {
         borderWidth: 8,
         borderColor: '#A65AFF87',
         backgroundColor: '#A65AFF',
         borderRadius: 100,
-        height:80,
-        width:80,
-        alignSelf: 'center',
-    },
-    galleryButton: {
-        position:'absolute',
-        alignItems:'center',
-        justifyContent:'center',
-        borderWidth:2,
-        borderColor: "#A65AFF",
-        borderRadius: 10,
-        overflow:"hidden",
-        width: 50,
-        height: 50,
-    },
-    galleyButtonImage: {
-        width: 50,
-        height: 50,
+        height: 80,
+        width: 80,
     },
     sideBarContainer: {
-        position:"absolute",
+        position: "absolute",
         top: 60,
-        right:0,
-        marginHorizontal:20,
+        right: 0,
+        marginHorizontal: 20,
     },
     sideBarButton: {
         alignContent: 'center',
@@ -64,9 +67,27 @@ const styles = StyleSheet.create({
     //     position:'absolute',
     //     display:'flex',
     // },
+
+
+    galleryButton: {
+        position: 'absolute',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 2,
+        borderColor: "#A65AFF",
+        borderRadius: 10,
+        overflow: "hidden",
+        width: 50,
+        height: 50,
+    },
+    galleyButtonImage: {
+        width: 50,
+        height: 50,
+    },
+
     video: {
-        height:'100%',
-        width:'100%',
+        height: '100%',
+        width: '100%',
 
     },
 })
