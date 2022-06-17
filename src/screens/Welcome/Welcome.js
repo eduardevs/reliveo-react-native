bb8cdcd0bfee31c63950baf56ff5ef44bfbaf3fc
jeseduardopi <jeseduardopi@gmail.com>
2022-06-17 14:49:52 +0200

    order in a folder (later this will be replaced)

--- src/screens/Welcome/Welcome.js
+++ src/screens/Welcome/Welcome.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { Image, Text, TouchableOpacity, View } from 'react-native'
 
-import { styles } from '../theme/styles/styleEduardo'
+import { styles } from '../../theme/styles/styleEduardo'
 // import {} from '../assets'
 
 const { Container, InnerContainer, PageTitle, StyledFormArea, SubTitle, StyledButton, ButtonText, Line, WelcomeContainer, Avatar, WelcomeImage } = styles
