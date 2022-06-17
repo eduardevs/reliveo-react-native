19ea7e501cfa8390219fe84992cbc48da0d14817
jeseduardopi <jeseduardopi@gmail.com>
2022-06-17 11:24:51 +0200

    Add new architecture

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import { StyleSheet, Text, View } from "react-native";
-import IndexPhoto from "./composents/camera/photo/indexPhoto";
-import IndexSynchro from "./composents/synchro/indexSynchro";
 import { RootStack } from "./navigators/RootStack";
 
 export default function App() {
