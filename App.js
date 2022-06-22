3b3465a1067835efd6ec7637f813e1877ce61ba7
jeseduardopi <jeseduardopi@gmail.com>
2022-06-22 19:33:10 +0200

    Add stack navigation

--- App.js
+++ App.js
@@ -1,17 +1,61 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { colors } from './src/theme/palette';
 import { useState } from 'react';
-import { View, ActivityIndicator } from 'react-native';
-import { RootStack } from './src/navigators/RootStack';
+import { View, ActivityIndicator, SafeAreaView, Text, StatusBar, Button } from 'react-native';
+// import { RootStack } from './src/navigators/RootStack';
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home screen</Text>
+            <Button
+                title="hello"
+                onPress={() => {
+                    navigation.navigate('Details');
+                }}
+            />
+        </View>
+    );
+};
+
+const DetailsScreen = ({ navigation }) => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Details</Text>
+            <Button
+                title="hello"
+                onPress={() => {
+                    navigation.navigate('Details');
+                }}
+            />
+        </View>
+    );
+};
+
+const { secondary } = colors;
 
 export default function App() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [userToken, setUserToken] = useState(null);
-
-    if (isLoading) {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <ActivityIndicator size="large" />
-            </View>
-        );
-    }
-    return <RootStack />;
+    // return <RootStack />;
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerStyled: {
+                        backgroundColor: 'transparent',
+                    },
+                    headerTintColor: secondary,
+                    headerTransparent: true,
+                    headerTitle: '',
+                    headerLeftContainerStyle: {
+                        paddingLeft: 20,
+                    },
+                }}
+            >
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
