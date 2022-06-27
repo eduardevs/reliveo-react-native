405ce3eca2a4a475cf85345a5c2531b8a30df635
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-06-27 23:32:01 +0200

    fixed view component

--- src/screens/profile/styles.js
+++ src/screens/profile/styles.js
@@ -2,7 +2,9 @@ import { StyleSheet } from 'react-native'
 import { ScreenHeight } from '../../theme/layout';
 
 const styles = StyleSheet.create({
-	
+	container:{
+    flex:1,
+  }
 });
 
 export default styles
