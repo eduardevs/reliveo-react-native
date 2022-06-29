0d6c69cc300477140864acd9d24436caf23e2ca6
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-06-29 22:52:30 +0200

    useffect profile

--- src/components/profile/profileHeader/index.js
+++ src/components/profile/profileHeader/index.js
@@ -1,14 +1,26 @@
 import { View, Text, Image } from 'react-native';
+import { useState, useEffect } from 'react';
 import React from 'react';
 import styles from './styles';
 
-export default function ProfileHeader({ name, email }) {
+export default function ProfileHeader({ userObject }) {
+    const data = [];
+    const [user, setUser] = useState(null);
+	const currentUser = user;
+    useEffect(() => {
+        if (currentUser == null) {
+            setUser(data);
+        }
+    });
+
+    console.log(user);
+	//case, switch avec des composants différents
     return (
         <View style={styles.container}>
             <Image style={styles.profile} source={require('../../../assets/gigachad.png')} />
-            <Text style={styles.nickname}>{name || 'GigaDunmer'}</Text>
-            <Text style={styles.hashtagfollowers}>{email || '@TribunallSuperior69'}</Text>
-            <Text style={styles.hashtagfollowers}>1.5M followers</Text>
+            <Text style={styles.nickname}>{user}</Text>
+            <Text style={styles.hashtagfollowers}>{user}</Text>
+            <Text style={styles.hashtagfollowers}>{user}</Text>
         </View>
     );
 }
