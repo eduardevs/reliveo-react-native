d5060fb7f2515e0bb6058e034566d291c8bafc57
jeseduardopi <jeseduardopi@gmail.com>
2022-07-03 13:57:31 +0200

    Retrieve dummie data

--- src/components/profile/profileHeader/index.js
+++ src/components/profile/profileHeader/index.js
@@ -1,17 +1,14 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import styles from './styles'
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+import styles from './styles';
 
-export default function ProfileHeader() {
-  return (
-    <View style={styles.container}>
-		<Image
-				style={styles.profile}
-				source={require("../../../assets/gigachad.png")}
-		/>
-			<Text style={styles.nickname}>GigaDunmer</Text>
-			<Text style={styles.hashtagfollowers}>@TribunallSuperior69</Text>
-			<Text style={styles.hashtagfollowers}>1.5M followers</Text>
-    </View>
-  )
+export default function ProfileHeader({ name, email }) {
+    return (
+        <View style={styles.container}>
+            <Image style={styles.profile} source={require('../../../assets/gigachad.png')} />
+            <Text style={styles.nickname}>{name || 'GigaDunmer'}</Text>
+            <Text style={styles.hashtagfollowers}>{email || '@TribunallSuperior69'}</Text>
+            <Text style={styles.hashtagfollowers}>1.5M followers</Text>
+        </View>
+    );
 }
--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -1,21 +1,47 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
-import React from 'react';
-import styles from "./styles";
+import { View, Text, StyleSheet, Image, Button } from 'react-native';
+import React, { useEffect, useContext, useState } from 'react';
+import styles from './styles';
 import ProfileNavBar from '../../components/profile/navBar';
 import ProfileHeader from '../../components/profile/profileHeader';
 import ProfileTabs from '../../components/profile/tabs/index';
 
-
-
+import { AuthContext } from '../../context/AuthContext';
 
 export const ProfileScreen = ({ navigation }) => {
-	
+    const { logout, userInfo } = useContext(AuthContext);
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    // useEffect(() => {
+    //     if (userInfo) {
+    //         console.log(userInfo);
+    //         console.log('it works');
+    //         console.log(userInfo.data);
+    //     }
+    // }, []);
+
+    useEffect(() => {
+        console.log('testttt', userInfo);
+        console.log('testeso', userInfo);
+
+        if (userInfo) {
+            const { data } = userInfo;
+            console.log(data);
+            if (data) {
+                const [user] = data;
+                console.log(user.name);
+                setName(user.name);
+                setEmail(user.email);
+            }
+        }
+    }, [userInfo]);
+
     return (
         <View style={styles.container}>
-			
             <ProfileNavBar />
-			<ProfileHeader />
-			<ProfileTabs />
+            <ProfileHeader name={name} email={email} />
+            <ProfileTabs />
+            <Button title="Log out" onPress={() => logout()} />
         </View>
     );
 };
