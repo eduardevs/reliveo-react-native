d386474342afdad32d7d6b29779a1882922eee48
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2022-07-03 18:25:14 +0200

    patch app

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-<<<<<<< HEAD
-import { AuthProvider } from './src/context/AuthContext';
-import { AppNav } from './src/navigators/AppNav';
-
-export default function App() {
-    return (
-        <AuthProvider>
-            <AppNav />
-        </AuthProvider>
-    );
-}
-=======
-import { RootStack } from "./src/navigators/RootStack";
 import BottomNav from "./src/containers/bottomNav/bottomNav";
 
 export default function App() {
-  return (
-      <BottomNav />
-)}
-
->>>>>>> bc1ab34 (patch import)
+    return (
+        <BottomNav/>
+    )
+}
