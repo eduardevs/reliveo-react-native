1b1f8ee0333c33e64df9f10755b8026fee2c2bf3
jeseduardopi <jeseduardopi@gmail.com>
2022-07-03 13:13:10 +0200

    clean imports

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
-import React, { useState } from 'react';
-import { colors } from './src/theme/palette';
-import { View, ActivityIndicator, SafeAreaView, Text, StatusBar, Button } from 'react-native';
-import { HomeStackScreen } from './src/navigators/HomeStackScreen';
-import { useEffect } from 'react';
-import { AuthContext, AuthProvider } from './src/context/AuthContext';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AuthProvider } from './src/context/AuthContext';
 import { AppNav } from './src/navigators/AppNav';
-
-const { secondary } = colors;
-
 export default function App() {
     return (
         <AuthProvider>
