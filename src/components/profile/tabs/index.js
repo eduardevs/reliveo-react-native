12abcc7577a10da2f3033d02e1c9039ad3de8aeb
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-07-05 17:01:24 +0200

    for into map + usestate on events

--- src/components/profile/tabs/index.js
+++ src/components/profile/tabs/index.js
@@ -1,163 +1,108 @@
-import { View, Text, Image } from "react-native";
-import React from "react";
-import styles from "./styles";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { ScrollView } from "react-native-gesture-handler";
-import { Ionicons } from "@expo/vector-icons";
+import { View, Text, Image, Animated, TouchableOpacity, TouchableHighlight } from 'react-native';
+import React, { useState } from 'react';
+import styles from './styles';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function ProfileTabs() {
-	const Tab = createMaterialTopTabNavigator();
+    const Tab = createMaterialTopTabNavigator();
+    const [events, setEvents] = useState([1, 2, 3, 4, 5, 6]);
 
-	let squares = [];
-	let numberOfSquares = 10;
-	let rectangles = [];
-	let numberOfRectangles = 10;
+    const Content = () => {
+        return (
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flexWrap: 'wrap',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        paddingTop: 30,
+                        backgroundColor: '#2E2E2E',
+                    }}
+                ></View>
+            </ScrollView>
+        );
+    };
 
-	for (let index = 0; index < numberOfSquares; index++) {
-		squares.push(
-			<View key={index}>
-				<View
-					style={{
-						width: 150,
-						height: 150,
-						backgroundColor: "black",
-						marginLeft: 10,
-						marginRight: 10,
-						marginBottom: 30,
-						borderBottomColor: "black",
-						borderWidth: 1,
-						borderRadius: 5,
-						position: "relative",
-					}}
-				>
-					<View
-						style={{
-							paddingTop: 2,
-							paddingBottom: 2,
-							paddingLeft: 5,
-							paddingRight: 20,
-							backgroundColor: "#3E3E3E",
-							marginLeft: 15,
-							marginRight: 15,
-							marginBottom: 30,
-							borderBottomColor: "black",
-							borderWidth: 1,
-							borderRadius: 5,
-							position: "absolute",
-							top: 15,
-						}}
-					>
-						<View style={{ flexDirection: "row" }}>
-							<Ionicons name="eye-outline" size={20} color="#FFFFFF" />
-							<Text
-								style={{ alignSelf: "center", color: "#FFFFFF", marginLeft: 5 }}
-							>
-								660k
-							</Text>
-						</View>
-						
-					</View>
-                    <View style={{
-                            width: 20,
-                            height: 20,
-							backgroundColor: "#3E3E3E",
-							position: "absolute",
-							bottom: 10,
-                            right: 10,
-						}}>
-							<MaterialIcons name="lock" size={20} color="#FFFFFF" />
-					</View>
-				</View>
-			</View>
-		);
-	}
+    const Favoris = () => {
+        return (
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flexWrap: 'wrap',
+                        alignItems: 'center',
+                        paddingTop: 30,
+                        backgroundColor: '#2E2E2E',
+                    }}
+                >
+                {events.map((event, index) => {
+                    const [activeBtn, setActiveBtn] = useState(false);
+                    return (
+                        <TouchableOpacity style={styles.carre}>
+                            <View style={styles.smallrect}>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <Ionicons name="eye-outline" size={20} color="#FFFFFF" />
+                                    <Text style={{ alignSelf: 'center', color: '#FFFFFF', marginLeft: 5 }}>660k</Text>
+                                </View>
+                            </View>
+                            <View style={styles.lock}>
+                                <MaterialIcons name="lock" size={20} color="#FFFFFF" />
+                            </View>
+                        </TouchableOpacity>
+                    );
+                })}
+                </View>
+            </ScrollView>
+        );
+    };
 
-	for (let index = 0; index < numberOfRectangles; index++) {
-		rectangles.push(
-			<View key={index}>
-				<View
-					style={{
-						width: 300,
-						height: 150,
-						backgroundColor: "black",
-						opacity: 0.1,
-						marginBottom: 30,
-						borderBottomColor: "black",
-						borderWidth: 1,
-						borderRadius: 17,
-					}}
-				></View>
-			</View>
-		);
-	}
+    const Events = () => {
+        return (
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        paddingTop: 30,
+                        backgroundColor: '#2E2E2E',
+                    }}
+                >
+                    {events.map((event, index) => {
+                        const [activeBtn, setActiveBtn] = useState(false);
+                        return (
+                            <TouchableOpacity
+                                key={index}
+                                onPress={() => {
+                                    setActiveBtn(!activeBtn);
+                                }}
+                            >
+                                <View
+                                    style={activeBtn ? styles.rectangleActive : styles.rectangle}
+                                    value={event}
+                                ></View>
+                            </TouchableOpacity>
+                        );
+                    })}
+                </View>
+            </ScrollView>
+        );
+    };
 
-	const Content = () => {
-		return (
-			<ScrollView showsVerticalScrollIndicator={false}>
-				<View
-					style={{
-						flexWrap: "wrap",
-						flexDirection: "row",
-						justifyContent: "center",
-						paddingTop: 30,
-						backgroundColor: "#2E2E2E",
-					}}
-				>
-					{squares}
-				</View>
-			</ScrollView>
-		);
-	};
-
-	const Favoris = () => {
-		return (
-			<ScrollView showsVerticalScrollIndicator={false}>
-				<View
-					style={{
-						flexWrap: "wrap",
-						flexDirection: "row",
-						justifyContent: "center",
-						paddingTop: 30,
-						backgroundColor: "#2E2E2E",
-					}}
-				>
-					{squares}
-				</View>
-			</ScrollView>
-		);
-	};
-
-	const Events = () => {
-		return (
-			<ScrollView showsVerticalScrollIndicator={false}>
-				<View
-					style={{
-						flexDirection: "column",
-						alignItems: "center",
-						paddingTop: 30,
-						backgroundColor: "#2E2E2E",
-					}}
-				>
-					{rectangles}
-				</View>
-			</ScrollView>
-		);
-	};
-
-	return (
-		<Tab.Navigator
-			screenOptions={{
-				tabBarStyle: { backgroundColor: "#2E2E2E", position: "relative" },
-				tabBarActiveTintColor: "#F2F2F2",
-				tabBarInactiveTintColor: "#C5C5C5",
-				tabBarIndicatorStyle: { backgroundColor: "#F2F2F2", position: "absolute"},
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarStyle: { backgroundColor: '#2E2E2E', position: 'relative' },
+                tabBarActiveTintColor: '#F2F2F2',
+                tabBarInactiveTintColor: '#C5C5C5',
+                tabBarIndicatorStyle: { backgroundColor: '#F2F2F2', position: 'absolute' },
                 // tabBarIndicatorContainerStyle: { position: "relative"}
-			}}
-		>
-			<Tab.Screen name="Content" component={Content} />
-			<Tab.Screen name="Favoris" component={Favoris} />
-			<Tab.Screen name="Events" component={Events} />
-		</Tab.Navigator>
-	);
+            }}
+        >
+            <Tab.Screen name="Content" component={Content} />
+            <Tab.Screen name="Favoris" component={Favoris} />
+            <Tab.Screen name="Events" component={Events} />
+        </Tab.Navigator>
+    );
 }
--- src/components/profile/tabs/styles.js
+++ src/components/profile/tabs/styles.js
@@ -1,8 +1,71 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native';
 import { ScreenHeight } from '../../../theme/layout';
 
 const styles = StyleSheet.create({
+    picture: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingTop: 30,
+        backgroundColor: '#2E2E2E',
+    },
+    rectangleActive: {
+        width: 300,
+        height: 150,
+        backgroundColor: 'black',
+        opacity: 0.1,
+        marginBottom: 30,
+        borderBottomColor: 'black',
+        borderWidth: 1,
+        borderRadius: 17,
+        transform: [{ translateX: -100 }],
+    },
+    rectangle: {
+        width: 300,
+        height: 150,
+        backgroundColor: 'black',
+        opacity: 0.1,
+        marginBottom: 30,
+        borderBottomColor: 'black',
+        borderWidth: 1,
+        borderRadius: 17,
+        transform: [{ translateX: 0 }],
+    },
     
+   carre:{
+        width: 170,
+        height: 170,
+        backgroundColor: 'black',
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 30,
+        borderBottomColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5,
+        position: 'relative',
+    },
+    smallrect: {
+            paddingTop: 2,
+            paddingBottom: 2,
+            paddingLeft: 5,
+            paddingRight: 20,
+            backgroundColor: '#3E3E3E',
+            marginLeft: 15,
+            marginRight: 15,
+            marginBottom: 30,
+            borderBottomColor: 'black',
+            borderWidth: 1,
+            borderRadius: 5,
+            position: 'absolute',
+            top: 15,
+    },
+    lock:{
+        width: 20,
+        height: 20,
+        backgroundColor: '#3E3E3E',
+        position: 'absolute',
+        bottom: 10,
+        right: 10,
+    },
 });
 
-export default styles
+export default styles;
