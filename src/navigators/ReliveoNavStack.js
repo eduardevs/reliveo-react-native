2e9a6dfc4fbfcaa67acb0d1496204feae5e8709d
Wylohn <wylohn.tribout@gmail.com>
2022-07-05 15:54:09 +0200

    route menu plus as screen

--- src/navigators/ReliveoNavStack.js
+++ src/navigators/ReliveoNavStack.js
@@ -1,11 +1,15 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { BottomNav } from '../containers/bottomNav/bottomNav';
+<<<<<<< HEAD
 import { EditProfile } from '../screens/EditProfile/editProfile';
 import { DiffuseurScreen} from '../screens/Diffuseur/diffuseurProfile';
 import { EvenementScreen} from '../screens/Evenement/evenementScreen';
 import IndexPhoto from '../containers/camera/photo/indexPhoto'
 import { HomeContainer } from '../containers/fileVideo/index';
 import { ProfileContainer } from '../containers/ProfileContainer';
+=======
+import { Home } from '../containers/index';
+>>>>>>> 48378f7 (route menu plus as screen)
 import { IndexPlus } from "../screens/MenuPlus/IndexPlus";
 import { Contact } from "../screens/MenuPlus/Contact";
 import { SettingsUser } from "../screens/MenuPlus/SettingsUser";
@@ -35,12 +39,17 @@ export const ReliveoNavStack = ({ navigation }) => {
             }}
             tabBar={(props) => <BottomNav {...props} />}
         >
+<<<<<<< HEAD
             <BottomStack.Screen name="Home" component={HomeContainer} />
             <BottomStack.Screen name="Profile" component={ProfileContainer} />
             <BottomStack.Screen name="EditProfile" component={EditProfile} />
             <BottomStack.Screen name="IndexPhoto" component={IndexPhoto} />
             <BottomStack.Screen name="DiffuseurScreen" component={DiffuseurScreen} />
             <BottomStack.Screen name="EvenementScreen" component={EvenementScreen} />
+=======
+            <BottomStack.Screen name="Home" component={Home} />
+            <BottomStack.Screen name="Profile" component={ProfileScreen} />
+>>>>>>> 48378f7 (route menu plus as screen)
             <BottomStack.Screen name="IndexPlus" component={IndexPlus} />
             <BottomStack.Screen name="InfoProject" component={InfoProject} />
             <BottomStack.Screen name="InfoApp" component={InfoApp} />
