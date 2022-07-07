14862f33f4e935d36b60e72646c3745bafd25e81
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-07-07 18:27:27 +0200

    page autre utilisateur

--- src/components/profile/navBar/index.js
+++ src/components/profile/navBar/index.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Dimensions } from 'react-native';
 import React, { useState } from 'react';
 import styles from './styles';
 import { Entypo } from '@expo/vector-icons';
@@ -7,9 +7,12 @@ import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function ProfileNavBar({ navigation }) {
-    const [animateBtn, setanimateBtn] = useState(true);
-    const [showPopup, setshowPopup] = useState(false);
-    const [iscurrentUser] = useState(true);
+
+    const [showPopup, setshowPopup] = useState(false)
+    const [showReportOption, setshowReportOption] = useState(false)
+    const [showAgreeReport, setshowAgreeReport] = useState(false)
+    const [iscurrentUser] = useState(true)
+    
 
     switch (iscurrentUser) {
         case true:
@@ -17,7 +20,6 @@ export default function ProfileNavBar({ navigation }) {
                 <View style={styles.container}>
                     <TouchableOpacity
                         onPress={() =>
-                            // setanimateBtn(!animateBtn)
                             navigation.goBack()
                         }
                         style={{ padding: 10, display: 'flex', alignItems: 'center' }}
@@ -26,10 +28,11 @@ export default function ProfileNavBar({ navigation }) {
                     </TouchableOpacity>
                     <TouchableOpacity
                         onPress={() => {
-                            setanimateBtn(!animateBtn);
+                           
                             setshowPopup(!showPopup);
                         }}
-                        style={animateBtn ? styles.gear : styles.gearPressed}
+                        style={styles.gear}
+
                     >
                         <FontAwesome name="gear" size={24} color="white" />
                     </TouchableOpacity>
@@ -53,7 +56,7 @@ export default function ProfileNavBar({ navigation }) {
                 <View style={styles.container}>
                     <TouchableOpacity
                         onPress={() =>
-                            // setanimateBtn(!animateBtn)
+
                             navigation.goBack()
                         }
                         style={{ padding: 10, display: 'flex', alignItems: 'center' }}
@@ -63,26 +66,86 @@ export default function ProfileNavBar({ navigation }) {
                     <TouchableOpacity
                         onPress={() => {
                             setanimateBtn(!animateBtn);
-                            console.log('test');
+
                             setshowPopup(!showPopup);
                         }}
                         style={animateBtn ? styles.gear : styles.gearPressed}
                     >
                         <FontAwesome5 name="ellipsis-v" size={24} color="white" />
                     </TouchableOpacity>
-                    <View
-                        style={{
-                            position: 'absolute',
-                            right: 20,
-                            padding: 10,
-                            top: 80,
-                        }}
-                    >
-                        <TouchableOpacity style={showPopup ? styles.popupcontainer : styles.popupnocontainer}>
+
+                    <View style={showPopup ? styles.popupcontainer : styles.popupnocontainer}>
+                        <TouchableOpacity
+                            onPress={() => {
+                                setshowReportOption(!showReportOption);
+                                setshowPopup(!showPopup);
+                            }}
+                            style={{ flexDirection: 'row', display: 'flex' }}
+                        >
+
                             <Ionicons name="flag-sharp" size={20} color="white" />
                             <Text style={styles.textpopup}>Signaler</Text>
                         </TouchableOpacity>
                     </View>
+
+                    <View style={showReportOption ? styles.reportOption : styles.noreportOption}>
+                        <View style={styles.reportButton}>
+                            <Text style={styles.reportButtonTitre}>Signalement</Text>
+                        </View>
+                        <TouchableOpacity
+                            onPress={() => {
+                                setshowAgreeReport(!showAgreeReport);
+                                setshowReportOption(!showReportOption);
+                            }}
+                            style={styles.reportButton}
+                        >
+                            <Text style={styles.reportButtonText}>Contenu à caractère sexuel</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => {
+                                setshowAgreeReport(!showAgreeReport);
+                                setshowReportOption(!showReportOption);
+                            }}
+                            style={styles.reportButton}
+                        >
+                            <Text style={styles.reportButtonText}>Contenu discriminant</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => {
+                                setshowAgreeReport(!showAgreeReport);
+                                setshowReportOption(!showReportOption);
+                            }}
+                            style={styles.reportButton}
+                        >
+                            <Text style={styles.reportButtonText}>Contenu sans lien avec l'évènement</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => {
+                                setshowAgreeReport(!showAgreeReport);
+                                setshowReportOption(!showReportOption);
+                            }}
+                            style={styles.reportButton}
+                        >
+                            <Text style={styles.reportButtonText}>Contenu offensant</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={showAgreeReport ? styles.accordMessage : styles.noaccordMessage}>
+                        <Text style={{ textAlign: 'center', color: 'white' }}>
+                            Votre signalement a bien été pris en compte.
+                        </Text>
+                        <Text style={{ textAlign: 'center', color: 'white' }}>
+                            Notre équipe de modération va analyser votre signalement dans les plus brefs délais.
+                        </Text>
+                        <TouchableOpacity
+                            onPress={() => {
+                                setshowAgreeReport(!showAgreeReport);
+                            }}
+                            style={styles.btnaccord}
+                        >
+                            <Text style={styles.btnaccordtext}>D'accord</Text>
+                        </TouchableOpacity>
+                    </View>
+
                 </View>
             );
     }
