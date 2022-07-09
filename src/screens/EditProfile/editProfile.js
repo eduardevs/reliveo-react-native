e08b5a5d2352aab8dd97eb42bdd182ad2eb9866b
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-07-09 13:08:52 +0200

    upload image

--- src/screens/EditProfile/editProfile.js
+++ src/screens/EditProfile/editProfile.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image, Button, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, Button, TouchableOpacity, ActivityIndicator } from 'react-native';
 import React, { useEffect, useContext, useState } from 'react';
 import styles from './styles';
 import BottomNav from '../../containers/bottomNav/bottomNav';
@@ -8,7 +8,8 @@ import ProfileNavBar from '../../components/profile/navBar';
 import { InputText } from '../../components/inputs/InputText/InputText';
 import { Feather } from '@expo/vector-icons';
 import Gallery from '../../containers/camera/Gallery';
-import IndexPhoto from '../../containers/camera/photo/indexPhoto'
+import IndexPhoto from '../../containers/camera/photo/indexPhoto';
+import { firebase } from '../../../firebaseConfig';
 
 const { Container, InnerContainer, PageTitle, StyledFormArea, SubTitle, StyledButton, ButtonText, Line } = styles;
 
@@ -18,8 +19,10 @@ export const EditProfile = ({ navigation }) => {
     const [privatise, setPrivatise] = useState(false);
     const [name, setName] = useState();
     const [email, setEmail] = useState();
-    const [showChoice, setshowChoice] = useState(false)
-    const [image, setImage] = useState()
+    const [showChoice, setshowChoice] = useState(false);
+    const [image, setImage] = useState();
+    const [urlimage, seturlImage] = useState();
+    const [uploding, setUploding] = useState(false);
 
     useEffect(() => {
         if (userInfo) {
@@ -34,36 +37,69 @@ export const EditProfile = ({ navigation }) => {
         }
     }, [userInfo]);
 
+    const updateProfile = () =>{
+        uplodPicture()
+    }
+
+    const uplodPicture = async () => {
+        const response = await fetch(image);
+        const blob = await response.blob();
+        console.log(image);
+        const filename = `photoProfil/photo${image.slice(-40, -4)}`;
+        const ref = firebase.storage().ref().child(filename).put(blob);
+        ref.on(
+            firebase.storage.TaskEvent.STATE_CHANGED,
+            () => {
+                setUploding(true);
+            },
+            (error) => {
+                setUploding(false);
+                console.log(error);
+                return;
+            },
+            () => {
+                ref.snapshot.ref.getDownloadURL().then((url) => {
+                    setUploding(false);
+                    console.log('download url : ' + url);
+                    blob.close();
+                    seturlImage(url)
+                    return ;
+                });
+            },
+        );
+    };
+
     return (
         <View style={styles.container}>
             <ProfileNavBar navigation={navigation} />
             <View>
                 <Text style={{ alignSelf: 'center', marginTop: 50, color: 'white' }}>Modifier Profile</Text>
             </View>
-            <TouchableOpacity 
-            onPress={() => {
-                setshowChoice(!showChoice);
-            }}
-            style={{ marginTop: 40, marginLeft: 35, height: 45, width: 50 }}>
-                <Image style={styles.profile} source={{uri:image}} />
+            <TouchableOpacity
+                onPress={() => {
+                    setshowChoice(!showChoice);
+                }}
+                style={{ marginTop: 40, marginLeft: 35, height: 45, width: 50 }}
+            >
+                <Image style={styles.profile} source={{ uri: image }} />
                 <Feather name="camera" size={20} color="white" style={{ position: 'relative', bottom: 16, left: 35 }} />
             </TouchableOpacity>
-            <View style={showChoice ? styles.pictureOption : styles.nopictureOption} >
+            <View style={showChoice ? styles.pictureOption : styles.nopictureOption}>
                 <TouchableOpacity
                     onPress={() => {
                         navigation.navigate('IndexPhoto');
                     }}
                     style={styles.reportButton}
                 >
-                    <Text style={{color:'white'}}>Prendre une nouvelle Photo</Text>
+                    <Text style={{ color: 'white' }}>Prendre une nouvelle Photo</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
                     onPress={() => {
-                        Gallery(setImage)
+                        Gallery(setImage);
                     }}
                     style={styles.reportButton}
                 >
-                    <Text style={{color:'white'}}>Accéder à votre Gallerie</Text>
+                    <Text style={{ color: 'white' }}>Accéder à votre Gallerie</Text>
                 </TouchableOpacity>
             </View>
             <View style={{ marginTop: 30, marginLeft: 35 }}>
@@ -87,16 +123,20 @@ export const EditProfile = ({ navigation }) => {
                     </TouchableOpacity>
                 </View>
             </View>
-            <TouchableOpacity
-                onPress={() => {
-                    setvalidateEdit(!validateEdit);
-                    navigation.navigate('Profile');
-                }}
-                style={styles.btnedit}
-            >
-                <Text style={styles.btnedittext}>Valider</Text>
-            </TouchableOpacity>
-            
+            {!uploding ? (
+                <TouchableOpacity
+                    onPress={() => {
+                        setvalidateEdit(!validateEdit);
+                        navigation.navigate('Profile');
+                        updateProfile()
+                    }}
+                    style={styles.btnedit}
+                >
+                    <Text style={styles.btnedittext}>Valider</Text>
+                </TouchableOpacity>
+            ) : (
+                <ActivityIndicator size="large" color="white" />
+            )}
         </View>
     );
 };
